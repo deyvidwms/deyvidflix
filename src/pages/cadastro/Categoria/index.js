787c179c0297aae457287e717091d59c7545b9e1
Alucardzx360 <deyvidwms@gmail.com>
2020-07-30 10:17:20 -0300

    Dividindo o amb. de produção do amb. de teste

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://deyvidflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (responseServer) => {
         const response = await responseServer.json();
